f6e01dfbd1801fdae66b6c5e3c6af1577c8ede96
lvanderspek <root@localhost.localdomain>
2019-11-17 00:26:09 -0800

    implemented bfs, printing final result

--- Regionals-PACNW-2019/C-ColoringContention/solution.cpp
+++ Regionals-PACNW-2019/C-ColoringContention/solution.cpp
@@ -2,10 +2,35 @@
 #include <vector>
 #include <list>
 #include <queue>
+#include <climits>
 using namespace std;
 
 
 typedef vector<list<int>> AdjList;
+
+
+void bfs(AdjList &graph, int start, vector<int> &distances) {
+    int n = graph.size();
+    vector<char> color(n, 'w');
+
+    color[start] = 'g';
+    distances[start] = 0;
+
+    queue<int> Q;
+    Q.push(start);
+    while (!Q.empty()) {
+        int cur = Q.front();
+        Q.pop();
+        
+        for (int to : graph[cur]) if (color[to] == 'w') {
+            Q.push(to);
+            color[to] = 'g';
+            distances[to] = distances[cur]+1;
+        }
+
+        color[cur] = 'b';
+    }
+}
 
 
 int main() {
@@ -20,4 +45,8 @@ int main() {
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
+
+    vector<int> distances(N, INT_MAX);
+    bfs(graph, 0, distances);
+    cout << distances[N-1]-1 << endl;
 }
