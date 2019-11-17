9d6c3861651d89e25f9d5686de89065eddbe2a42
lvanderspek <lvanderspek@mail.csuchico.edu>
2019-11-17 00:48:00 -0800

    printing solution

---
+++ Regionals-PACNW-2019/D-DividingByTwo/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    int ops = 0;
+    while (A>B) {
+        if (A%2) {
+            A++;
+            ops++;
+            continue;
+        }
+        if (A>B) {
+            A/=2;
+            ops++;
+            continue;
+        }
+        if (A<B) {
+            ops+=B-A;
+            break;
+        }
+    }
+
+    cout << ops << endl;
+}
