99332a90617bbc27421628d2c6165eadd408a44c
root <root@localhost.localdomain>
2019-11-16 19:42:30 -0800

    maze can now be normalized

--- Regionals-PACNW-2019/M-MazeConnect/solution.cpp
+++ Regionals-PACNW-2019/M-MazeConnect/solution.cpp
@@ -6,6 +6,14 @@ using namespace std;
 typedef vector<vector<char>> Maze;
 
 
+// linear time but that's ok
+void push_front(vector<char> &v, char c) {
+    v.resize(v.size()+1);
+    for (int i = v.size()-1; i; i--) v[i] = v[i-1];
+    v[0] = c;
+}
+
+
 void readMaze(Maze &maze, int R, int C) {
     maze.resize(R);
     for (int r = 0; r < R; r++) {
@@ -21,12 +29,36 @@ void printMaze(Maze &maze) {
     }
 }
 
+bool isEven(Maze &maze) {
+    int R = maze.size();
+    int C = maze[0].size();
+    for (int r = 0; r < R; r++) {
+        for (int c = 0; c < C; c++) {
+            char chr = maze[r][c];
+            if (chr=='.') continue;
+            if (chr== '/') return (r+c)%2 == 0;
+            if (chr=='\\') return (r+c)%2 == 1;
+        }
+    }
+    return true; // empty graph is even
+}
+
+void normalizeMaze(Maze &maze) {
+    if (!isEven(maze))
+        for (auto &row : maze) push_front(row, '.');
+
+    if (maze[0].size() % 2 == 1)
+        for (auto &row : maze) row.push_back('.');
+}
+
 int main() {
     int R, C;
     Maze maze;
 
     cin >> R >> C;
     readMaze(maze, R, C);
+
+    normalizeMaze(maze);
 
     printMaze(maze);
 }
