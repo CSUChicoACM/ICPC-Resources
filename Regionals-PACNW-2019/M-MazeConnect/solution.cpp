662e8ecb79a681b98feceea0b710b889ce9d413e
root <root@localhost.localdomain>
2019-11-16 19:15:04 -0800

    cleaned up maze io

--- Regionals-PACNW-2019/M-MazeConnect/solution.cpp
+++ Regionals-PACNW-2019/M-MazeConnect/solution.cpp
@@ -6,22 +6,27 @@ using namespace std;
 typedef vector<vector<char>> Maze;
 
 
-void print_line(vector<char> &v) {
-    for (char &c : v) cout << c;
-    cout << endl;
-}
-
-int main() {
-    int R, C;
-    cin >> R >> C;
-
-    Maze maze;
+void readMaze(Maze &maze, int R, int C) {
     maze.resize(R);
-
     for (int r = 0; r < R; r++) {
         maze[r].resize(C);
         for (int c = 0; c < C; c++) cin >> maze[r][c];
     }
+}
 
-    for (auto &v : maze) print_line(v);
+void printMaze(Maze &maze) {
+    for (auto &row : maze) {
+        for (char &c : row) cout << c;
+        cout << endl;
+    }
+}
+
+int main() {
+    int R, C;
+    Maze maze;
+
+    cin >> R >> C;
+    readMaze(maze, R, C);
+
+    printMaze(maze);
 }
