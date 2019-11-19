be322305191433abe5cd056ae6604144664170fa
lvanderspek <lvanderspek@mail.csuchico.edu>
2019-11-18 16:55:54 -0800

    found path length for every pair of nodes

--- Regionals-PACNW-2019/A-RadioPrize/solution.cpp
+++ Regionals-PACNW-2019/A-RadioPrize/solution.cpp
@@ -4,22 +4,31 @@
 using namespace std;
 
 
-typedef struct { int to; int w; } edge;
-typedef vector<list<edge>> AdjList;
+typedef  struct { int to; int w; }  edge;
+typedef  vector<list<edge>>         AdjList;
+typedef  vector<vector<int>>        Matrix;
 
 
-void dfsVisit(AdjList &graph, vector<char> &color, int cur, int depth) {
-    for (int i = 0; i < depth; i++) printf("  "); printf("%d\n", cur);
-    color[cur] = 'g';
-    for (edge &e : graph[cur]) if (color[e.to]=='w') {
-        dfsVisit(graph, color, e.to, depth+1);
+void dfsVisit(AdjList &graph, Matrix &distances, list<int> &seen, int cur, int parent) {
+    seen.push_back(cur);
+    for (edge &e : graph[cur]) if (e.to != parent) {
+        printf("%d->%d: ", cur, e.to);
+        for (int s : seen) {
+            int w = distances[cur][s] + e.w;
+            printf("{%d,%d,%d} ", s, e.to, w);
+            distances[s][e.to] = w;
+            distances[e.to][s] = w;
+        }
+        printf("\n");
+        dfsVisit(graph, distances, seen, e.to, cur);
     }
-    color[cur] = 'b';
 }
 
-void dfs(AdjList &graph) {
-    vector<char> color(graph.size(), 'w');
-    dfsVisit(graph, color, 0, 0);
+void getDistances(AdjList &graph, Matrix &distances) {
+    int n = graph.size();
+    distances.resize( n, vector<int>(n,0) ); // nXn matrix of zeroes
+    list<int> seen;
+    dfsVisit(graph, distances, seen, 0, -1);
 }
 
 int main() {
@@ -44,5 +53,13 @@ int main() {
         printf("\n");
     }
 
-    dfs(graph);
+    Matrix distances;
+    getDistances(graph, distances);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf("%2d ", distances[i][j]);
+        printf("\n");
+    }
+
 }
