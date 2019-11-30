c69c353bb96add5deb7fe314290d1de0624c9ebd
lvanderspek <lvanderspek@mail.csuchico.edu>
2019-11-30 09:53:21 -0800

    visiting edges

--- Regionals-PACNW-2019/A-RadioPrize/solution.cpp
+++ Regionals-PACNW-2019/A-RadioPrize/solution.cpp
@@ -4,33 +4,37 @@
 using namespace std;
 
 
-typedef  struct { int to; int w; }  edge;
+typedef  struct { int from; int to; int w; }  edge;
 typedef  vector<list<edge>>         AdjList;
 
 
-void getDistances(AdjList &graph) {
-    int n = graph.size();
-    distances.resize( n, vector<int>(n,0) ); // nXn matrix of zeroes
-    list<int> seen;
-    dfsVisit(graph, distances, seen, 0, -1);
+void processEdge(AdjList &graph, vector<int> &tax, edge &e1) {
+    printf("%d--%d-->%d\n", e1.from, e1.w, e1.to);
+    for (edge &e2 : graph[e1.to]) if (e2.to != e1.from) {
+        processEdge(graph, tax, e2);
+    }
+}
+
+void getDistances(AdjList &graph, vector<int> &tax) {
+    edge e = {-1, 0, 0};
+    processEdge(graph, tax, e);
 }
 
 int main() {
     int N, u, v, w;
 
     cin >> N;
-    vector<int> tax(N);
-    AdjList graph(N);
 
+    vector<int> tax(N);
     for (int &t : tax) cin >> t;
 
+    AdjList graph(N);
     for (int i = 0; i < N-1; i++) {
         cin >> u >> v >> w;
         u--, v--;
-        graph[u].push_back({v,w});
-        graph[v].push_back({u,w});
+        graph[u].push_back({u, v,w});
+        graph[v].push_back({v, u,w});
     }
 
-    Matrix distances;
-    getDistances(graph);
+    getDistances(graph, tax);
 }
