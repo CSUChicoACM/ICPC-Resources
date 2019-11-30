05ada0710d378d04eda46ae6157649777d9d282c
lvanderspek <lvanderspek@mail.csuchico.edu>
2019-11-30 10:19:59 -0800

    solved for first node

--- Regionals-PACNW-2019/A-RadioPrize/solution.cpp
+++ Regionals-PACNW-2019/A-RadioPrize/solution.cpp
@@ -4,21 +4,46 @@
 using namespace std;
 
 
-typedef  struct { int from; int to; int w; }  edge;
-typedef  vector<list<edge>>         AdjList;
+typedef  struct {
+    int from;
+    int to;
+    int w;
+    int nn;  // number of nodes
+    int ttm; // total tax multiplier
+    int tel; // total edge length
+    int ttr; // total tax returned
+}  edge;
+
+typedef vector<list<edge>> AdjList;
 
 
 void processEdge(AdjList &graph, vector<int> &tax, edge &e1) {
-    printf("%d--%d-->%d\n", e1.from, e1.w, e1.to);
+    if (e1.tel != -1) return;
+
+    e1.nn = e1.ttm = e1.tel = e1.ttr = 0;
     for (edge &e2 : graph[e1.to]) if (e2.to != e1.from) {
         processEdge(graph, tax, e2);
+        e1.nn  += e2.nn;
+        e1.ttm += e2.ttm;
+        e1.tel += e2.tel;
+        e1.ttr += e2.ttr;
     }
+
+    e1.nn  += 1;
+    e1.ttm += tax[e1.to];
+    e1.tel += e1.nn  * e1.w;
+    e1.ttr += e1.ttm * e1.w;
+
+    printf("%d--%d-->%d\n", e1.from, e1.w, e1.to);
+    printf("ttm:%d\ntel:%d\nttr:%d\nnn:%d\n----------------\n\n", e1.ttm, e1.tel, e1.ttr, e1.nn);
 }
 
 void getDistances(AdjList &graph, vector<int> &tax) {
-    edge e = {-1, 0, 0};
+    edge e = {-1, 0, 0, -1, -1, -1, -1};
     processEdge(graph, tax, e);
+    printf("%d\n", e.ttr + (e.tel*tax[0]));
 }
+
 
 int main() {
     int N, u, v, w;
@@ -32,8 +57,8 @@ int main() {
     for (int i = 0; i < N-1; i++) {
         cin >> u >> v >> w;
         u--, v--;
-        graph[u].push_back({u, v,w});
-        graph[v].push_back({v, u,w});
+        graph[u].push_back({u, v, w, -1, -1, -1, -1});
+        graph[v].push_back({v, u, w, -1, -1, -1, -1});
     }
 
     getDistances(graph, tax);
